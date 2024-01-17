d2030b5202ca448807c88b6c87bc46978c7aa2e1
reza <rzxsism80@gmail.com>
2024-01-16 16:47:20 -0800

    implementation completed

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from classification.classification import Classification
-from preprocessing.preprocessing import PreProcessingHandler
+from menu import InputHandler
 
 if __name__ == "__main__":
-    pre_pro = PreProcessingHandler()
-    classification = Classification()
-    [print(classification.classify_test_set_sentence(j)) for i in classification.extract_train_set() for j in i]
+    input_handler = InputHandler()
+    input_handler.menu_handler()
